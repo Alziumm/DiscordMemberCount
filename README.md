311c52017da77ff55a783ce63f94933a36cb029b
Alzium <pi.lebret06@gmail.com>
2023-02-07 21:39:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Very very simple python script to get the number of members of a discord server.
+
+This function can be use to get the number of members of a discord server.
+I made this function as part of a django application. I hope it will be useful to you.
+
+<strong>HOW TO USE :</strong>
+
+-> git clone ...
+-> pip install -r requirements.txt
+-> Use : `get_member_count(https://discord.com/invite/exemple)` 
+
+## Creator :
+
+* Alzium : https://github.com/Alziumm/
---
+++ main.py
@@ -0,0 +1,44 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+def extract_numbers(text : str) -> str or None:
+
+    match = re.search(r'\b\d{1,3}(,\d{3})*\b', text)
+
+    if match != True:
+
+        return None
+    
+    return match.group(0)
+
+def get_member_count(invite_link : str) -> str:
+
+    if "https://discord.com/" not in invite_link and "https://discord.gg/" not in invite_link:
+
+        return "NULL"
+
+    try:
+        response = requests.get(invite_link)
+    except:
+        return "NULL"
+
+    if response.status_code != 200:
+        
+        return "NULL"
+    
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+        member_count_tag = soup.find("meta", property="og:description")
+        data = extract_numbers(str(member_count_tag))
+    except:
+        return "NULL"
+
+    if data is None or data == "" or len(data) == 0:
+
+        return "NULL"
+
+    return data
+
+member_count = get_member_count("https://discord.gg/exemple")
+
+print(member_count)
